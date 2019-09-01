1a2972d72b15ae00e4ec588b417d371b9f581599
Mario Villegas <mraffov@gmail.com>
2019-08-31 21:46:19 -0400

    Commit inicial, componente de posts listo

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <router-link :to="{ name: 'Posts' }" class="navbar-brand">
+        Post
+      </router-link>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link :to="{ name: 'CreatePost' }" class="nav-link">
+              Create Post
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!-- CONTENT -->
+    <div class="container">
+      <div>
+        <transition name="fade">
+          <router-view></router-view>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Posts.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="text-center">Posts JsonPlaceholder</h1>
+    </div>
+    <div v-if="posts.length !== 0" class="row">
+      <div v-for="post in posts" class="card col-11 col-sm-11 col-lg-6 mt-3">
+        <div class="card-body">
+          <h5 class="card-title">
+            {{post.title}}
+          </h5>
+          <p class="card-text">
+            {{post.body}}
+          </p>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      <h3 class="text-center">Loading...</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Posts',
+  data () {
+    return {
+      posts : []
+    }
+  },
+
+  created: function () {
+    this.getAxiosPosts();
+  },
+
+  methods: {
+    async getAxiosPosts()
+    {
+      let uri = 'https://jsonplaceholder.typicode.com/posts';
+      await this.axios.get(uri).then((response) => {
+        this.posts =  response.data;
+      });
+    },
+  }
+}
+</script>
+
+
+<style>
+
+</style>
